01131d5972313a2366ff923c67fbf9954d8799cd
simon511 <simon.ding511@gmail.com>
2013-07-07 16:11:30 +0800

    update insertHistoryDataFromFile.py

--- insertHistoryDataFromFile.py
+++ insertHistoryDataFromFile.py
@@ -1,24 +1,23 @@
 from stocks.models import Stock, StockHistoryTransaction
 from django.utils import timezone
-import sys,urllib,urllib2
-import sys
+import sys,urllib,urllib2 
 
 
-files =os.listdir(r"e:\data2")
+files =os.listdir(r"e:\data")
 for file in files:
-    s = Stock.objects.filter(code=file[0:6])
-    tranData=StockHistoryTransaction(code=s.code,name=s.name)
-    fileHandle=open(r"e:\data2\\"+s.code+".txt")
+    s = Stock.objects.get(code=file[0:6])
+    fileHandle=open(r"e:\data\\"+s.code+".txt")
+    print s.code
     while 1:
-        line = fileHandle.readline
+        line = fileHandle.readline()
         if not line:
             break
         sData = line.split(",")
-        print sData
-
-'''
-       if StockHistoryTransaction.objects.filter(code=s.code,transactionDate=sData[8]).count()==0:
-            
+        if sData[8].find("2004-") ==0 :
+            print s.code," finished "
+            break
+        tranData=StockHistoryTransaction(code=s.code,name=s.name)
+        if StockHistoryTransaction.objects.filter(code=s.code,transactionDate=sData[8]).count()==0 :
             tranData.openingPrice=sData[2]
             tranData.closingPrice=sData[3]
             tranData.peakPrice=sData[4]
@@ -28,8 +27,9 @@ for file in files:
             tranData.transactionDate=sData[8]
             tranData.save()
         else:
-            print "already exists"
+            print "already exists",s.code,sData[8]
+            
+print "finished"
 
-'''
 
 
